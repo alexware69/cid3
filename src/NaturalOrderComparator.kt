32947bbe410d91d02957a4372a8d629e6b60f32c
Alejandro <alejandro.penate@me.com>
2021-04-30 22:22:38 -0400

    converted NaturalOrderComparator to Kotlin

--- src/NaturalOrderComparator.kt
+++ src/NaturalOrderComparator.kt
@@ -1,205 +1,156 @@
-/*
- NaturalOrderComparator.java -- Perform 'natural order' comparisons of strings in Java.
- Copyright (C) 2003 by Pierre-Luc Paour <natorder@paour.com>
+import java.io.Serializable
+import java.util.*
+import kotlin.jvm.JvmStatic
 
- Based on the C version by Martin Pool, of which this is more or less a straight conversion.
- Copyright (C) 2000 by Martin Pool <mbp@humbug.org.au>
-
- This software is provided 'as-is', without any express or implied
- warranty.  In no event will the authors be held liable for any damages
- arising from the use of this software.
-
- Permission is granted to anyone to use this software for any purpose,
- including commercial applications, and to alter it and redistribute it
- freely, subject to the following restrictions:
-
- 1. The origin of this software must not be misrepresented; you must not
- claim that you wrote the original software. If you use this software
- in a product, an acknowledgment in the product documentation would be
- appreciated but is not required.
- 2. Altered source versions must be plainly marked as such, and must not be
- misrepresented as being the original software.
- 3. This notice may not be removed or altered from any source distribution.
- */
-
-import java.io.Serializable;
-import java.util.*;
-
-public class NaturalOrderComparator implements Comparator, Serializable
-        {
-    int compareRight(String a, String b)
-    {
-        int bias = 0, ia = 0, ib = 0;
+class NaturalOrderComparator : Comparator<Any?>, Serializable {
+    private fun compareRight(a: String, b: String): Int {
+        var bias = 0
+        var ia = 0
+        var ib = 0
 
         // The longest run of digits wins. That aside, the greatest
         // value wins, but we can't know that it will until we've scanned
         // both numbers to know that they have the same magnitude, so we
         // remember it in BIAS.
-        for (;; ia++, ib++)
-        {
-            char ca = charAt(a, ia);
-            char cb = charAt(b, ib);
-
+        while (true) {
+            val ca = charAt(a, ia)
+            val cb = charAt(b, ib)
             if (!isDigit(ca) && !isDigit(cb)) {
-                return bias;
+                return bias
             }
             if (!isDigit(ca)) {
-                return -1;
+                return -1
             }
             if (!isDigit(cb)) {
-                return +1;
+                return +1
             }
-            if (ca == 0 && cb == 0) {
-                return bias;
+            if (ca.toInt() == 0 && cb.toInt() == 0) {
+                return bias
             }
-
             if (bias == 0) {
                 if (ca < cb) {
-                    bias = -1;
+                    bias = -1
                 } else if (ca > cb) {
-                    bias = +1;
+                    bias = +1
                 }
             }
+            ia++
+            ib++
         }
     }
 
-    public int compare(Object o1, Object o2)
-    {
-        String a = o1.toString();
-        String b = o2.toString();
-
-        int ia = 0, ib = 0;
-        int nza = 0, nzb = 0;
-        char ca, cb;
-
+    override fun compare(o1: Any?, o2: Any?): Int {
+        val a = o1.toString()
+        val b = o2.toString()
+        var ia = 0
+        var ib = 0
+        var nza: Int
+        var nzb: Int
+        var ca: Char
+        var cb: Char
         while (true) {
             // Only count the number of zeroes leading the last number compared
-            nza = nzb = 0;
-
-            ca = charAt(a, ia);
-            cb = charAt(b, ib);
+            nzb = 0
+            nza = nzb
+            ca = charAt(a, ia)
+            cb = charAt(b, ib)
 
             // skip over leading spaces or zeros
             while (Character.isSpaceChar(ca) || ca == '0') {
                 if (ca == '0') {
-                    nza++;
+                    nza++
                 } else {
                     // Only count consecutive zeroes
-                    nza = 0;
+                    nza = 0
                 }
-
-                ca = charAt(a, ++ia);
+                ca = charAt(a, ++ia)
             }
-
             while (Character.isSpaceChar(cb) || cb == '0') {
                 if (cb == '0') {
-                    nzb++;
+                    nzb++
                 } else {
                     // Only count consecutive zeroes
-                    nzb = 0;
+                    nzb = 0
                 }
-
-                cb = charAt(b, ++ib);
+                cb = charAt(b, ++ib)
             }
 
             // Process run of digits
             if (Character.isDigit(ca) && Character.isDigit(cb)) {
-                int bias = compareRight(a.substring(ia), b.substring(ib));
+                val bias = compareRight(a.substring(ia), b.substring(ib))
                 if (bias != 0) {
-                    return bias;
+                    return bias
                 }
             }
-
-            if (ca == 0 && cb == 0) {
+            if (ca.toInt() == 0 && cb.toInt() == 0) {
                 // The strings compare the same. Perhaps the caller
                 // will want to call strcmp to break the tie.
-                return compareEqual(a, b, nza, nzb);
+                return compareEqual(a, b, nza, nzb)
             }
             if (ca < cb) {
-                return -1;
+                return -1
             }
             if (ca > cb) {
-                return +1;
+                return +1
             }
-
-            ++ia;
-            ++ib;
+            ++ia
+            ++ib
         }
     }
-    
-    static boolean isDigit(char c) {
-        return Character.isDigit(c) || c == '.' || c == ',';
-    }
 
-    static char charAt(String s, int i) {
-        return i >= s.length() ? 0 : s.charAt(i);
-    }
-    
-    static int compareEqual(String a, String b, int nza, int nzb) {
-        if (nza - nzb != 0)
-            return nza - nzb;
+    companion object {
+        fun isDigit(c: Char): Boolean {
+            return Character.isDigit(c) || c == '.' || c == ','
+        }
 
-        if (a.length() == b.length())
-            return a.compareTo(b);
+        fun charAt(s: String, i: Int): Char {
+            return if (i >= s.length) '\u0000' else s[i]
+        }
 
-        return a.length() - b.length();
-    }
+        fun compareEqual(a: String, b: String, nza: Int, nzb: Int): Int {
+            if (nza - nzb != 0) return nza - nzb
+            return if (a.length == b.length) a.compareTo(b) else a.length - b.length
+        }
 
-    public static void main(String[] args)
-    {
-        String[] strings = new String[] { "1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
-            "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05", "pic 5",
-            "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
-            "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8" };
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val strings = arrayOf("1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
+                    "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05", "pic 5",
+                    "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
+                    "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8")
+            val orig: List<*> = listOf(*strings)
+            println("Original: $orig")
+            val scrambled: List<*> = listOf(*strings)
+            Collections.shuffle(scrambled)
+            println("Scrambled: $scrambled")
+            Collections.sort(scrambled, NaturalOrderComparator())
+            println("Sorted: $scrambled")
+            shuffle3000(scrambled)
+            compareSymmetric()
+            floatsWithCommas()
+        }
 
-        List orig = Arrays.asList(strings);
+        private fun shuffle3000(scrambled: List<Any?>) {
+            Collections.shuffle(scrambled, Random(3000))
+            Collections.sort(scrambled, NaturalOrderComparator())
+            println("Sorted: $scrambled")
+        }
 
-        System.out.println("Original: " + orig);
+        private fun compareSymmetric() {
+            val naturalOrderComparator = NaturalOrderComparator()
+            var compare1 = naturalOrderComparator.compare("1-2", "1-02")
+            var compare2 = naturalOrderComparator.compare("1-02", "1-2")
+            println("$compare1 == $compare2")
+            compare1 = naturalOrderComparator.compare("pic 5", "pic05")
+            compare2 = naturalOrderComparator.compare("pic05", "pic 5")
+            println("$compare1 == $compare2")
+        }
 
-        List scrambled = Arrays.asList(strings);
-        Collections.shuffle(scrambled);
-
-        System.out.println("Scrambled: " + scrambled);
-
-        Collections.sort(scrambled, new NaturalOrderComparator());
-
-        System.out.println("Sorted: " + scrambled);
-
-        shuffle3000(scrambled);
-
-        compareSymmetric();
-        
-        floatsWithCommas();
-    }
-    
-    static void shuffle3000(List<? extends Object> scrambled) {
-        Collections.shuffle(scrambled, new Random(3000));
-        Collections.sort(scrambled, new NaturalOrderComparator());
-
-        System.out.println("Sorted: " + scrambled);
-    }
-
-    static void compareSymmetric() {
-        NaturalOrderComparator naturalOrderComparator = new NaturalOrderComparator();
-
-        int compare1 = naturalOrderComparator.compare("1-2", "1-02");
-        int compare2 = naturalOrderComparator.compare("1-02", "1-2");
-
-        System.out.println(compare1 + " == " + compare2);
-
-        compare1 = naturalOrderComparator.compare("pic 5", "pic05");
-        compare2 = naturalOrderComparator.compare("pic05", "pic 5");
-
-        System.out.println(compare1 + " == " + compare2);
-    }
-    
-    static void floatsWithCommas() {
-        List<String> unSorted = Arrays.asList("0.9", "1.0c", "1.2", "1.3", "0.6", "1.1", "0.7", "0.3", "1.0b", "1.0", "0.8");
-
-        System.out.println("Unsorted: " + unSorted);
-
-        unSorted.sort(new NaturalOrderComparator());
-
-        System.out.println("Sorted: " + unSorted);
+        private fun floatsWithCommas() {
+            val unSorted = Arrays.asList("0.9", "1.0c", "1.2", "1.3", "0.6", "1.1", "0.7", "0.3", "1.0b", "1.0", "0.8")
+            println("Unsorted: $unSorted")
+            unSorted.sortWith(NaturalOrderComparator())
+            println("Sorted: $unSorted")
+        }
     }
 }
