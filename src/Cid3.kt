4ca8db0c12811aefd65962a1725a476c9a80caad
Alejandro <alejandro.penate@me.com>
2021-05-02 05:32:54 -0400

    formatted the code a little bit

--- src/Cid3.kt
+++ src/Cid3.kt
@@ -2794,18 +2794,18 @@ class Cid3 : Serializable {
         } else println("The file doesn't exist.")
     }
 
-    companion object {
-        fun formatDuration(duration: Duration): String {
-            val seconds = duration.seconds
-            val absSeconds = abs(seconds)
-            val positive = String.format(
-                    "%d:%02d:%02d",
-                    absSeconds / 3600,
-                    absSeconds % 3600 / 60,
-                    absSeconds % 60)
-            return if (seconds < 0) "-$positive" else positive
-        }
+    private fun formatDuration(duration: Duration): String {
+        val seconds = duration.seconds
+        val absSeconds = abs(seconds)
+        val positive = String.format(
+                "%d:%02d:%02d",
+                absSeconds / 3600,
+                absSeconds % 3600 / 60,
+                absSeconds % 60)
+        return if (seconds < 0) "-$positive" else positive
+    }
 
+    companion object {
         /* Here is the definition of the main function */
         @JvmStatic
         fun main(args: Array<String>) {
