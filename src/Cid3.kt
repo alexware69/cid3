79ed4a97f2ab84c3e5b3b2319c68cdb70a316539
Alejandro <alejandro.penate@me.com>
2021-10-15 22:52:58 -0400

    replaced * for ✓

--- src/Cid3.kt
+++ src/Cid3.kt
@@ -1116,7 +1116,7 @@ class Cid3 : Serializable {
         //Stop the animation
         runAnimationReadingTest = false
 
-        print("\r" + "[ ☺ ] " + "Read data: " + testData.size + " cases for testing. ")
+        print("\r" + "[ ✓ ] " + "Read data: " + testData.size + " cases for testing. ")
         print("\n")
     }
 
@@ -1228,11 +1228,11 @@ class Cid3 : Serializable {
         runAnimationReading = false
 
         if (splitTrainData && !testDataExists) {
-            print("\r" + "[ ☺ ] " + "Read data: " + root.data.size + " cases for training. ($na attributes)")
+            print("\r" + "[ ✓ ] " + "Read data: " + root.data.size + " cases for training. ($na attributes)")
             print("\n")
-            print("\r" + "[ ☺ ] " + "Read data: " + testData.size + " cases for testing.")
+            print("\r" + "[ ✓ ] " + "Read data: " + testData.size + " cases for testing.")
         } else {
-            print("\r" + "[ ☺ ] " + "Read data: " + root.data.size + " cases for training. ($na attributes)")
+            print("\r" + "[ ✓ ] " + "Read data: " + root.data.size + " cases for training. ($na attributes)")
         }
         print("\n")
     } // End of function readData
@@ -1504,7 +1504,7 @@ class Cid3 : Serializable {
 
         //Stop the animation
         runAnimationCalculating = false
-        print("\r" + "[ ☺ ] " + "Decision tree created.")
+        print("\r" + "[ ✓ ] " + "Decision tree created.")
 
         print("\n")
         print("\n")
@@ -1651,7 +1651,7 @@ class Cid3 : Serializable {
         //Stop the animation
         runAnimationCalculating = false
 
-        print("\r" + "[ ☺ ] " + "10-fold cross-validation created with " + root.data.size + " cases.")
+        print("\r" + "[ ✓ ] " + "10-fold cross-validation created with " + root.data.size + " cases.")
 
         print("\n")
         testCrossValidation()
@@ -1728,7 +1728,7 @@ class Cid3 : Serializable {
         }
         //Stop the animation
         runAnimationCalculating = false
-        print("\r" + "[ ☺ ] " + "10-fold Random Forests cross-validation created with " + root.data.size + " cases.")
+        print("\r" + "[ ✓ ] " + "10-fold Random Forests cross-validation created with " + root.data.size + " cases.")
 
         print("\n")
 
@@ -1783,7 +1783,7 @@ class Cid3 : Serializable {
         if (!cv) {
             //Stop the animation
             runAnimationCalculating = false
-            print("\r" + "[ ☺ ] " + "Random Forest of " + roots.size + " trees created.")
+            print("\r" + "[ ✓ ] " + "Random Forest of " + roots.size + " trees created.")
 
             print("\n")
             val sortedList: List<Triple<Int, Double, Double>> = if (criteria == Criteria.Certainty)
