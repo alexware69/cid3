c33d86435f282062b52868383d922303168f95f8
Alejandro <alejandro.penate@me.com>
2021-10-26 22:05:55 -0400

    updated version number

--- src/Cid3.kt
+++ src/Cid3.kt
@@ -14,7 +14,7 @@ import kotlin.system.exitProcess
 
 
 class Cid3 : Serializable {
-    private val version = "1.0.3"
+    private val version = "1.0.4"
     private var createdWith = ""
     enum class AttributeType {
         Discrete, Continuous, Ignore
